76f5d12163911e43c8be7c4293dcc099364eabbd
Hugo Gibbs <hugo.d.gibbs@gmail.com>
2022-11-09 01:54:29 -0300

    .

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # weather-app
+In this weather app you can search any location around the world and you will be able to see instantly its temperature, description, humidity and wind speed.
+
+This app uses Promises with JavaScript to fetch data using the API from www.openweatherapp.org.
+
+Also you will see everytime a different background image related to the location you are searching.
--- script.js
+++ script.js
@@ -1,9 +1,44 @@
-let weather = document.querySelector('img');
+let weather = {
+    apiKey: "a77fe1a0aeeed44c29856e7253db881c",
+    fetchWeather: function(city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + city 
+            + "&units=metric&appid=" 
+            + this.apiKey
+        )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { temp, humidity } = data.main;
+        const { icon, description } = data.weather[0];
+        const { speed } = data.wind;
+        console.log(name, temp, humidity, speed)
+        document.querySelector(".city").innerText = name;
+        document.querySelector(".temp").innerText = temp + " °C";
+        document.querySelector(".icon").src = 
+            "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind: " + speed + " km/h";
+        document.querySelector(".weatherBox").classList.remove("loading");
+        document.querySelector(".contentBox").style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector("input.text").value);
+    }
+};
 
-async function getWeather() {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${Search}&APPID=a77fe1a0aeeed44c29856e7253db881c`, {mode: 'cors'})
-    const weatherData = await response.json();
-    img.src = weatherData.data.images.original.url;
-  }
+document.querySelector("button.search").addEventListener("click", function () {
+    weather.search();
+});
 
-  getWeather();
+document.querySelector("input.text").addEventListener("keypress", function (e) {
+    if (e.key == "Enter") {
+        weather.search();
+    }
+});
+
+weather.fetchWeather("Vancouver");
